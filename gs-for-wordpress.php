a3675a4e00f511edbc5068d17a4fe750df6437dd
Gigya-inc <Gigya-inc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-08 15:36:29 +0000

    git-svn-id: http://plugins.svn.wordpress.org/gigya-socialize-for-wordpress/trunk@250027 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gs-for-wordpress.php
+++ gs-for-wordpress.php
@@ -4,7 +4,7 @@
  Plugin URI: http://gigya.com
  Description: Integrates a variety of features of the Gigya Socialize service into a WordPress blog.
  Author: Nick Ohrn of Plugin-Developer.com
- Version: 1.2.2alpha
+ Version: 1.2.3
  Author URI: http://plugin-developer.com
  */
 
