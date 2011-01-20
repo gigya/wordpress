57f7a44cfebd65e0ff2c19b211843ef409dfdad1
Gigya-inc <Gigya-inc@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-20 11:00:03 +0000

    git-svn-id: http://plugins.svn.wordpress.org/gigya-socialize-for-wordpress/trunk@334968 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/comment-notification.php
+++
@@ -1,9 +0,0 @@
-<?php /** The variables $usersComment are available here. **/ ?>
-<script type="text/javascript">
-jQuery(document).ready(function() { 
-	var theStatus = '<?php echo $status; ?>';
-	if( typeof( gigya ) != 'undefined' ) {
-		gigya.services.socialize.setStatus(gigyaSocializeGeneralConfiguration, {status:theStatus});
-	}
-});
-</script>
