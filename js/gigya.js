914857c4126ee35b50de3f0261d5c3e7c51f274d
gigya.com <gigya.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-01 06:29:28 +0000

    Merged security fix
    git-svn-id: http://plugins.svn.wordpress.org/gigya-socialize-for-wordpress/trunk@780743 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/gigya.js
+++ js/gigya.js
@@ -1,209 +1,207 @@
 // Gigya Namespace
 window.Gigya = {};
 
-jQuery(document).ready(function($) {
-	// Template Manager
-    $.fn.serializeObject = function()
-{
-   var o = {};
-   var a = this.serializeArray();
-   $.each(a, function() {
-       if (o[this.name]) {
-           if (!o[this.name].push) {
-               o[this.name] = [o[this.name]];
-           }
-           o[this.name].push(this.value || '');
-       } else {
-           o[this.name] = this.value || '';
-       }
-   });
-   return o;
-};
-	Gigya.Tmpl = function(target){
-		this.target = target;
-		return this;
-	};
-	
-	Gigya.Tmpl.prototype = {
-		target : null,
-		renderLoginDialog: function(data){
-			this.header(data).newUser(data);
-			if(data.linkAccount) this.linkAccount(data);
-			$('#gigya-new-user-wrap').wrap('<div class="ui-helper-clearfix" style="margin-bottom:15px"/>').parent()
-			.append($('#gigya-sep-wrap'))
-			.append($('#gigya-account-linking-wrap'));
-		},
-		tmpl : function(tplId,params,onAppend){
-			$("#"+tplId).tmpl(params || {}).appendTo(this.target);
-			if(onAppend) onAppend.call(this,params);
-			return this;	
-		},
-		newUser: function(params){
-			this.tmpl("gigya-new-user-tmpl",params,function(){
-				$("#gigya-new-user-button").click(function(){
-					Gigya.Ajax.login({
-						actionType : "register-email", 
-						email      : $("#gigya-new-user-wrap input[name=email]").val()
-					});
-				});
-			});
-			return this;
-		},
-		linkAccount: function(params){
-			this.tmpl("gigya-account-linking-tmpl",params,function(){
-				$("#gigya-new-account-button").click(function(){
-					Gigya.Ajax.login({
-						actionType : "link-account", 
-						email      : $("#gigya-account-linking-wrap input[name=email]").val(),
-						password   : $("#gigya-account-linking-wrap input[name=password]").val()
-					});
-				});
-			});
-			return this;
-		},
-		header: function(params){
-			this.tmpl("gigya-header-tmpl",params,$.noop);
-			return this;
-		},
-        renderForm: function(params){
-            $('#login-dialog').html(params.html);
-            $('#gigya-ajax-submit').click( function(e) {
-                e.preventDefault();
-                var action = 
-                console.log( $(this).parents('form').eq(0).serializeObject() );
-                
+jQuery(document).ready(function ($) {
+    // Template Manager
+    $.fn.serializeObject = function () {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+    Gigya.Tmpl = function (target) {
+        this.target = target;
+        return this;
+    };
+
+    Gigya.Tmpl.prototype = {
+        target: null,
+        renderLoginDialog: function (data) {
+            this.header(data).newUser(data);
+            if (data.linkAccount) this.linkAccount(data);
+            $('#gigya-new-user-wrap').wrap('<div class="ui-helper-clearfix" style="margin-bottom:15px"/>').parent()
+                .append($('#gigya-sep-wrap'))
+                .append($('#gigya-account-linking-wrap'));
+        },
+        tmpl: function (tplId, params, onAppend) {
+            $("#" + tplId).tmpl(params || {}).appendTo(this.target);
+            if (onAppend) onAppend.call(this, params);
+            return this;
+        },
+        newUser: function (params) {
+            this.tmpl("gigya-new-user-tmpl", params, function () {
+                $("#gigya-new-user-button").click(function () {
+                    Gigya.Ajax.login({
+                        actionType: "register-email",
+                        email: $("#gigya-new-user-wrap input[name=email]").val()
+                    });
+                });
             });
-        } 
-	}
-	// Error Manager
-	Gigya.Error =  function(){
-		return {
-			show: function(text){
-				try {
-					this.hide();
-					var show_in_dialog = $("#dialog-header").length>0,target,markup;
-					if(show_in_dialog){
-						target = $("#dialog-header");
-						markup = "<p class='message error' style='display:none;'>"+text+"<br></p>";
-					} else {
-						target =  $("#login h1");
-						markup = "<div id='login_error'>"+text+"<br></div>";
-					}
-					
-					target.after(markup).parent().find("p.message").fadeIn();
-				} catch(e) {
-					
-				}
-				 
-			},
-			hide: function(){
-				try {
-					var show_in_dialog = $("#dialog-header").length>0,target,markup;
-					var target = $("#dialog-header").length>0 ? $("#dialog-header").parent().find(".message") :  $("#login_error,p.message");
-					target.remove();
-				} catch(e) {
-					
-				}
-			}
-		}
-	}();
-	// Login Ajax
-	Gigya.Ajax = function(){
-		var userObject = {};
-		var adminurl = "";
-		return {
-			onSignIn: $.noop,
-			setUrl: function(url){
-				adminurl = url;
-			},
-			setUserObject: function(obj){
-				userObject = obj;
-				if(userObject.user.firstName == "") userObject.user.firstName = userObject.user.nickname;
-			},
-			login: function(data){
-				var that = this;
-				Gigya.Error.hide();
-				$.post(adminurl,$.extend({
-					userObject:JSON.stringify(userObject),
-					action: "gigya_user_login",
+            return this;
+        },
+        linkAccount: function (params) {
+            this.tmpl("gigya-account-linking-tmpl", params, function () {
+                $("#gigya-new-account-button").click(function () {
+                    Gigya.Ajax.login({
+                        actionType: "link-account",
+                        email: $("#gigya-account-linking-wrap input[name=email]").val(),
+                        password: $("#gigya-account-linking-wrap input[name=password]").val()
+                    });
+                });
+            });
+            return this;
+        },
+        header: function (params) {
+            this.tmpl("gigya-header-tmpl", params, $.noop);
+            return this;
+        },
+        renderForm: function (params) {
+            $('#login-dialog').html(params.html);
+            $('#gigya-ajax-submit').click(function (e) {
+                e.preventDefault();
+                var action =
+                    console.log($(this).parents('form').eq(0).serializeObject());
+
+            });
+        }
+    }
+    // Error Manager
+    Gigya.Error = function () {
+        return {
+            show: function (text) {
+                try {
+                    this.hide();
+                    var show_in_dialog = $("#dialog-header").length > 0, target, markup;
+                    if (show_in_dialog) {
+                        target = $("#dialog-header");
+                        markup = "<p class='message error' style='display:none;'>" + text + "<br></p>";
+                    } else {
+                        target = $("#login h1");
+                        markup = "<div id='login_error'>" + text + "<br></div>";
+                    }
+
+                    target.after(markup).parent().find("p.message").fadeIn();
+                } catch (e) {
+
+                }
+
+            },
+            hide: function () {
+                try {
+                    var show_in_dialog = $("#dialog-header").length > 0, target, markup;
+                    var target = $("#dialog-header").length > 0 ? $("#dialog-header").parent().find(".message") : $("#login_error,p.message");
+                    target.remove();
+                } catch (e) {
+
+                }
+            }
+        }
+    }();
+    // Login Ajax
+    Gigya.Ajax = function () {
+        var userObject = {};
+        var adminurl = "";
+        return {
+            onSignIn: $.noop,
+            setUrl: function (url) {
+                adminurl = url;
+            },
+            setUserObject: function (obj) {
+                userObject = obj;
+                if (userObject.user.firstName == "") userObject.user.firstName = userObject.user.nickname;
+            },
+            login: function (data) {
+                var that = this;
+                Gigya.Error.hide();
+                $.post(adminurl, $.extend({
+                    userObject: JSON.stringify(userObject),
+                    action: "gigya_user_login",
                     step: 1
-				},data),function(r){
-					try {
-						//var r = JSON.parse(r);
-						switch(r.type) {
-							case "email_exist":
-								new Gigya.Dialog().open(function(){
-									new Gigya.Tmpl(this).renderLoginDialog($.extend(userObject,{
-										isEmailExist  :true,
-										linkAccount   : (r.params.account_linking && r.params.force_email) 
-									}));
-										
-								});
-							break; 
-							case "new_user_email_required":
-								new Gigya.Dialog().open(function(){
-									new Gigya.Tmpl(this).renderLoginDialog($.extend(userObject,{
-										isNewUser    :true,
-										isEmailExist :false,
-										linkAccount  : (r.params.account_linking && r.params.force_email) 
-									}));	
-								});	
-							break;
+                }, data), function (r) {
+                    try {
+                        //var r = JSON.parse(r);
+                        switch (r.data.type) {
+                            case "email_exist":
+                                new Gigya.Dialog().open(function () {
+                                    new Gigya.Tmpl(this).renderLoginDialog($.extend(userObject, {
+                                        isEmailExist: true,
+                                        linkAccount: (r.data.params.account_linking && r.data.params.force_email)
+                                    }));
+
+                                });
+                                break;
+                            case "new_user_email_required":
+                                new Gigya.Dialog().open(function () {
+                                    new Gigya.Tmpl(this).renderLoginDialog($.extend(userObject, {
+                                        isNewUser: true,
+                                        isEmailExist: false,
+                                        linkAccount: (r.data.params.account_linking && r.data.params.force_email)
+                                    }));
+                                });
+                                break;
                             case "reg_form":
                                 new Gigya.Dialog().open(function () {
                                     new Gigya.Tmpl(this).renderForm($.extend({}, r, userObject));
                                 })
-                            
-	 
-							case "error":
-							Gigya.Error.show(r.text);
-							break; 
-							case "signin":
-								that.onSignIn.call(this,r,userObject);
-							break;
-						}
-					} catch(e) {
-                        console.log(e);
-			
-					}
-				});
-			}
-		}
-	}();
-	
-	// Dialog
-	Gigya.Dialog = function(){
-		if($("#login-dialog").length==0) $("body").append("<div id='login-dialog'></div>");
-		this.target = $("#login-dialog");
-		return this;
-	};
-	 
-	Gigya.Dialog.prototype = {
-		open: function(beforeOpen){
-			var that = this,beforeOpen = beforeOpen || $.noop;
-			// render markup for dialog before open
-			that.target.empty();
-			beforeOpen.call(that.target);
-			// open dialog
-			$("#login-dialog").dialog($.extend({
-				title: "Login",
-				modal: true,
-				width:'auto',
-				resizable: false,
-				zIndex   : 100000,
-				close: function(){
-					that.target.empty();
-					$(this).dialog("destroy");	
-				},
-				buttons: {
-					Cancel: function() {
-			          $( this ).dialog( "close" );
-			        }
-			    }
-			},{})).dialog("open");
-			
-			;
-		}
-	};
+
+
+                            case "error":
+                                Gigya.Error.show(r.data.text);
+                                break;
+                            case "signin":
+                                that.onSignIn.call(this, r.data, userObject);
+                                break;
+                        }
+                    } catch (e) {
+
+                    }
+                });
+            }
+        }
+    }();
+
+    // Dialog
+    Gigya.Dialog = function () {
+        if ($("#login-dialog").length == 0) $("body").append("<div id='login-dialog'></div>");
+        this.target = $("#login-dialog");
+        return this;
+    };
+
+    Gigya.Dialog.prototype = {
+        open: function (beforeOpen) {
+            var that = this, beforeOpen = beforeOpen || $.noop;
+            // render markup for dialog before open
+            that.target.empty();
+            beforeOpen.call(that.target);
+            // open dialog
+            $("#login-dialog").dialog($.extend({
+                title: "Login",
+                modal: true,
+                width: 'auto',
+                resizable: false,
+                zIndex: 100000,
+                close: function () {
+                    that.target.empty();
+                    $(this).dialog("destroy");
+                },
+                buttons: {
+                    Cancel: function () {
+                        $(this).dialog("close");
+                    }
+                }
+            }, {})).dialog("open");
+
+            ;
+        }
+    };
 });
 	
