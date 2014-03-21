1238930d2329c2b29332def1476e1dead3aa13cf
Raz Konforti <konforti@gmail.com>
2014-03-21 12:46:24 +0200

    forms

--- admin/admin.GigyaSettings.php
+++ admin/admin.GigyaSettings.php
@@ -20,37 +20,37 @@ class GigyaSettings {
 				'gigya_global_settings'    => array(
 						'title' => 'Global Settings',
 						'func'  => 'globalSettingsForm',
-						'slug'  => 'gigya-global'
+						'slug'  => 'gigya_global_settings'
 				),
 				'gigya_login_settings'     => array(
 						'title' => 'Social Login Settings',
 						'func'  => 'loginSettingsForm',
-						'slug'  => 'gigya-social-login'
+						'slug'  => 'gigya_login_settings'
 				),
 				'gigya_share_settings'     => array(
 						'title' => 'Share Settings',
 						'func'  => 'shareSettingsForm',
-						'slug'  => 'gigya-share'
+						'slug'  => 'gigya_share_settings'
 				),
 				'gigya_comments_settings'  => array(
 						'title' => 'Comments Settings',
 						'func'  => 'commentsSettingsForm',
-						'slug'  => 'gigya-comments'
+						'slug'  => 'gigya_comments_settings'
 				),
 				'gigya_reactions_settings' => array(
 						'title' => 'Reactions Settings',
 						'func'  => 'reactionsSettingsForm',
-						'slug'  => 'gigya-reactions'
+						'slug'  => 'gigya_reactions_settings'
 				),
 				'gigya_gm_settings'        => array(
 						'title' => 'Gamification Settings',
 						'func'  => 'gmSettingsForm',
-						'slug'  => 'gigya-gm'
+						'slug'  => 'gigya_gm_settings'
 				),
 				'gigya_raas_settings'      => array(
 						'title' => 'RASS Settings',
 						'func'  => 'raasSettingsForm',
-						'slug'  => 'gigya-raas'
+						'slug'  => 'gigya_raas_settings'
 				),
 		);
 	}
@@ -62,12 +62,12 @@ class GigyaSettings {
 	public function gigya_admin_menu() {
 
 		// Register the main Gigya setting route page.
-		add_menu_page( 'Gigya', 'Gigya', GIGYA__PERMISSION_LEVEL, 'gigya-global', array($this, '_gigya_admin_page'), plugin_dir_url( __FILE__ ) . 'images/favicon_28px.png', '70.1' );
+		add_menu_page( 'Gigya', 'Gigya', GIGYA__PERMISSION_LEVEL, 'gigya_global_settings', array($this, '_gigya_admin_page'), plugin_dir_url( __FILE__ ) . 'assets/images/favicon_28px.png', '70.1' );
 
 		// Register the sub-menus Gigya setting pages.
 		foreach ($this->getSections() as $section) {
 			require_once( GIGYA__PLUGIN_DIR . 'admin/forms/' . $section['func'] . '.php' );
-			add_submenu_page( 'gigya-global', __( $section['title'], $section['title'] ), __( $section['title'], $section['title'] ), GIGYA__PERMISSION_LEVEL, $section['slug'], array($this, '_gigya_admin_page') );
+			add_submenu_page( 'gigya_global_settings', __( $section['title'], $section['title'] ), __( $section['title'], $section['title'] ), GIGYA__PERMISSION_LEVEL, $section['slug'], array($this, '_gigya_admin_page') );
 		}
 	}
 
@@ -84,7 +84,7 @@ class GigyaSettings {
 		// Add settings sections.
 		foreach ($this->getSections() as $id => $section) {
 			add_settings_section( $id, $section['title'], $section['func'], $section['slug'] );
-			register_setting( $section['slug'] . '-settings-group', $section['slug'] . '-settings' );
+			register_setting( $section['slug'] . '-group', $section['slug'] );
 		}
 	}
 
@@ -101,7 +101,7 @@ class GigyaSettings {
 		echo '<form class="gigya-settings" action="options.php" method="post">';
 
 		 	wp_nonce_field( 'update-options' );
-		 	settings_fields( $page . '-settings-group' );
+		 	settings_fields( $page . '-group' );
 		 	do_settings_sections( $page );
 		 	submit_button();
 
@@ -123,7 +123,7 @@ class GigyaSettings {
 		foreach ( $form as $el ) {
 			// Add a section param. to be added to the element attribute NAME.
 			// Tells to WP under which option to save this field.
-			$el['section'] = $_GET['page'] . '-settings';
+			$el['section'] = $_GET['page'];
 
 			// Render each element.
 			$render .= _gigya_render_tpl( 'admin/tpl/formEl-' . $el['type'] . '.tpl.php', $el );
--- admin/tpl/adminPage-wrapper.tpl.php
+++ admin/tpl/adminPage-wrapper.tpl.php
@@ -12,13 +12,13 @@
 	</div>
 
 	<div class="nav-tab-wrapper">
-		<a href="?page=gigya-global" class="nav-tab <?php echo $page == 'gigya-global' ? 'nav-tab-active' : ''; ?>">Global Settings</a>
-		<a href="?page=gigya-social-login" class="nav-tab <?php echo $page == 'gigya-social-login' ? 'nav-tab-active' : ''; ?>">Social Login Settings</a>
-		<a href="?page=gigya-share" class="nav-tab <?php echo $page == 'gigya-share' ? 'nav-tab-active' : ''; ?>">Share Settings</a>
-		<a href="?page=gigya-comments" class="nav-tab <?php echo $page == 'gigya-comments' ? 'nav-tab-active' : ''; ?>">Comments Settings</a>
-		<a href="?page=gigya-reactions" class="nav-tab <?php echo $page == 'gigya-reactions' ? 'nav-tab-active' : ''; ?>">Reactions Settings</a>
-		<a href="?page=gigya-gm" class="nav-tab <?php echo $page == 'gigya-gm' ? 'nav-tab-active' : ''; ?>">Gamification Settings</a>
-		<a href="?page=gigya-raas" class="nav-tab <?php echo $page == 'gigya-raas' ? 'nav-tab-active' : ''; ?>">RAAS Settings</a>
+		<a href="?page=gigya_global_settings" class="nav-tab <?php echo $page == 'gigya_global_settings' ? 'nav-tab-active' : ''; ?>">Global Settings</a>
+		<a href="?page=gigya_login_settings" class="nav-tab <?php echo $page == 'gigya_login_settings' ? 'nav-tab-active' : ''; ?>">Social Login Settings</a>
+		<a href="?page=gigya_share_settings" class="nav-tab <?php echo $page == 'gigya_share_settings' ? 'nav-tab-active' : ''; ?>">Share Settings</a>
+		<a href="?page=gigya_comments_settings" class="nav-tab <?php echo $page == 'gigya_comments_settings' ? 'nav-tab-active' : ''; ?>">Comments Settings</a>
+		<a href="?page=gigya_reactions_settings" class="nav-tab <?php echo $page == 'gigya_reactions_settings' ? 'nav-tab-active' : ''; ?>">Reactions Settings</a>
+		<a href="?page=gigya_gm_settings" class="nav-tab <?php echo $page == 'gigya_gm_settings' ? 'nav-tab-active' : ''; ?>">Gamification Settings</a>
+		<a href="?page=gigya_raas_settings" class="nav-tab <?php echo $page == 'ggigya_raas_settings' ? 'nav-tab-active' : ''; ?>">RAAS Settings</a>
 	</div>
 
 	<?php
