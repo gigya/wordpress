5eab7aa01046bd5894f2bfce62f413abdd8b51f6
Raz Konforti <konforti@gmail.com>
2014-03-21 21:31:22 +0200

    login

--- assets/scripts/gigya_login.js
+++ assets/scripts/gigya_login.js
@@ -1,16 +1,66 @@
-(function ($) {
-	$(document).ready(function() {
-		// Changing the default look and behavior of the plugin using the 'params' object:
-		var params = {
+( function ( $ ) {
+	var GigyaWp = GigyaWp || {};
 
-			// The plugin will embed itself inside the "loginDiv" DIV (will not be a popup)
-			containerID:"gigya-login",
+// --------------------------------------------------------------------
 
-			// After successful login - the user will be redirected to "https://www.MySite.com/welcome.html" :
-			redirectURL: "https://www.MySite.com/welcome.html"
+	gigya.socialize.showLoginUI({
+
+		// The plugin will embed itself inside the "loginDiv" DIV (will not be a popup)
+		containerID: "gigya-login",
+
+		// After successful login - the user will be redirected to "https://www.MySite.com/welcome.html" :
+		//			redirectURL: "https://www.MySite.com/welcome.html"
+	});
+
+// --------------------------------------------------------------------
+
+	GigyaWp.login = function (data) {
+
+		var options = {
+			url: gigyaLoginParams.ajaxurl,
+			type: 'POST',
+			dataType: 'json',
+			data:  {
+				data: data,
+				nonce: gigyaLoginParams.nonce
+			}
+		};
+
+		$.ajax(options)
+				.done(function(data) {
+					console.log(data);
+				});
+	}
+
+	GigyaWp.loginCallback = function ( response ) {
+		if ( response.provider === 'site' ) {
+			return false;
 		}
 
+		if ( ( response.user.email.length === 0 ) && ( response.user.isSiteUID !== true ) ) {
+			var email = prompt( "Please fill-in missing details" );
 
-		gigya.socialize.showLoginUI(params);
+			if ( email == null ) {
+				// User clicked Cancel.
+				gigya.socialize.logout();
+			}
+			else {
+				// User clicked OK.
+				// TODO add validation: empty string, email chars ...
+				response.user.email = email;
+			}
+		}
+
+		GigyaWp.login( response );
+	}
+
+	GigyaWp.logoutCallback = function (response) {
+		alert(response.eventName + " event happened");
+	}
+
+	gigya.socialize.addEventHandlers({
+		onLogin : GigyaWp.loginCallback,
+		onLogout: GigyaWp.logoutCallback
 	});
-})(jQuery);s
+})(jQuery);
+
--- gigya.php
+++ gigya.php
@@ -46,11 +46,10 @@ function _gigya_init_action() {
 	wp_enqueue_script( 'jquery' );
 
 	// Gigya configuration values.
-	$login_options = get_option( GIGYA__SETTINGS_LOGIN );
+	$login_options  = get_option( GIGYA__SETTINGS_LOGIN );
 	$global_options = get_option( GIGYA__SETTINGS_GLOBAL );
 
 	if ( ! empty( $login_options['login_plugin'] ) && ! empty( $global_options['global_api_key'] ) ) {
-
 		// Load Gigya's socialize.js.
 		wp_enqueue_script( 'gigya', 'http://cdn.gigya.com/JS/socialize.js?apiKey=' . $global_options['global_api_key'], 'jquery' );
 	}
@@ -69,24 +68,84 @@ function _gigya_init_action() {
  */
 add_action( 'login_form', '_gigya_login_form_action' );
 function _gigya_login_form_action() {
-
-	// Load custom Gigya login script.
-	wp_enqueue_script( 'gigya_login_js', plugins_url( 'assets/scripts/gigya_login.js', __FILE__ ) );
-	wp_enqueue_style( 'gigya_login_css', plugins_url( 'assets/styles/gigya_login.css', __FILE__ ) );
-
 	// Gigya configuration values.
-	$values = get_option( GIGYA__SETTINGS_LOGIN );
+	$login_options  = get_option( GIGYA__SETTINGS_LOGIN );
+	$global_options = get_option( GIGYA__SETTINGS_GLOBAL );
 
-	// Parameters to be sent to DOM.
-	$params = array(
-			'socialLogin' => $values['login_plugin'],
-	);
+	if ( ! empty( $login_options['login_plugin'] ) && ! empty( $global_options['global_api_key'] ) ) {
 
-	// Load params to be available to client-side script.
-	wp_localize_script( 'gigya_login_js', 'gigyaLoginParams', $params );
-	require_once( GIGYA__PLUGIN_DIR . 'class/class.GigyaLoginFormAction.php' );
+		// Add an element to attach the Gigya Login UI to.
+		echo '<div id="gigya-login"></div>';
 
-	echo '<div id="gigya-login"></div>';
+		// Load custom Gigya login script.
+		wp_enqueue_script( 'gigya_login_js', plugins_url( 'assets/scripts/gigya_login.js', __FILE__ ) );
+		wp_enqueue_style( 'gigya_login_css', plugins_url( 'assets/styles/gigya_login.css', __FILE__ ) );
+
+		// Parameters to be sent to the DOM.
+		$params = array(
+				'ajaxurl' => admin_url( 'admin-ajax.php' ),
+				'nonce'   => wp_create_nonce( 'gigya-login-ajax-nonce' ),
+		);
+
+		// Load params to be available to client-side script.
+		wp_localize_script( 'gigya_login_js', 'gigyaLoginParams', $params );
+		require_once( GIGYA__PLUGIN_DIR . 'class/class.GigyaLoginAction.php' );
+	}
+}
+
+// --------------------------------------------------------------------
+
+add_action( 'wp_ajax_nopriv_gigya_login', 'ajax_gigya_login_callback' );
+function ajax_gigya_login_callback() {
+
+	// check to see if the submitted nonce matches with the
+	// generated nonce we created earlier
+	$nonce = $_POST['nonce'];
+	if ( ! wp_verify_nonce( $nonce, 'gigya-login-ajax-nonce' ) ) {
+		die ( 'Busted!' );
+	}
+
+	$data           = $_POST['data'];
+	$login_redirect = user_admin_url();
+	$login_options  = get_option( GIGYA__SETTINGS_LOGIN );
+
+	if ( ! empty ( $login_options['login_redirect'] ) ) {
+		$login_redirect = $login_options['login_redirect'];
+	}
+
+	if ( ! is_user_logged_in() ) {
+		$global_options = get_option( GIGYA__SETTINGS_GLOBAL );
+		if ( SigUtils::validateUserSignature( $data['UID'], $data['timestamp'], $global_options['global_secret_key'], $data['signature'] ) ) {
+			$gigya_user = $data['user'];
+
+			// Check to see if the Gigya user is a WP user.
+			if ( is_numeric( $gigya_user['UID'] ) && $data['isSiteUID'] === 'true' && is_object( $user = get_userdata( $gigya_user['UID']) )) {
+
+				// Login the user.
+				wp_clear_auth_cookie();
+				wp_set_current_user ( $user->ID, $user->user_login );
+				wp_set_auth_cookie  ( $user->ID );
+
+				do_action( 'wp_login', $user->user_login );
+
+
+			}
+			else {
+				// If the user isn't a WP user, try to register if allowed.
+				$user_id = wp_create_user( $gigya_user['firstName'] . $gigya_user['lastName'], wp_generate_password(), $gigya_user['email'] );
+
+				do_action( 'user_register', $user_id );
+			}
+		}
+	}
+
+	$response = json_encode( array( 'success' => true ) );
+	header( "Content-Type: application/json" );
+	echo $response;
+
+	wp_safe_redirect( $login_redirect );
+
+	exit;
 }
 
 // --------------------------------------------------------------------
@@ -96,8 +155,6 @@ function _gigya_login_form_action() {
  */
 add_action( 'wp_enqueue_scripts', '_gigya_wp_enqueue_scripts_action' );
 function _gigya_wp_enqueue_scripts_action() {
-
-
 
 
 }
