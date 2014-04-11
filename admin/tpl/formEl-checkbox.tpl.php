38d1efc1da0fb84e55da2f53905f0d27dcf561d2
Raz Konforti <konforti@gmail.com>
2014-04-11 16:41:46 +0300

    forms

--- admin/tpl/formEl-checkbox.tpl.php
+++ admin/tpl/formEl-checkbox.tpl.php
@@ -6,7 +6,6 @@
 ?>
 <div class="row checkbox <?php echo $class ?>">
 	<label for="gigya_<?php echo $id; ?>">
-		<input type="hidden" value="0" id="gigya_<?php echo $id; ?>" name="<?php echo $name ?>" />
 		<input type="checkbox" <?php echo( $value || $value == "1" ? "checked" : "" ); ?> value="1" id="gigya_<?php echo $id; ?>" name="<?php echo $name ?>" />
 		<?php echo $label; ?>
 	</label>
