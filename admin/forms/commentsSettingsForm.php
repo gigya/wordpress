6938c94e46683af11e02526ef3d7ad65fea80898
Raz Konforti <konforti@gmail.com>
2014-04-24 16:06:43 +0300

    fix settings pages

--- admin/forms/commentsSettingsForm.php
+++ admin/forms/commentsSettingsForm.php
@@ -12,27 +12,29 @@ function commentsSettingsForm() {
 			'value' => $values['on'] === '0' ? '0' : '1'
 	);
 
-	$form['position'] = array(
-			'type'    => 'select',
-			'options' => array(
-					'under' => __( 'Under Post' ),
-					'none'  => __( 'None' )
-			),
-			'label'   => __( 'Comments position' ),
-			'value'   => _gigParam( $values['position'], 'under' ),
-	);
-
 	$form['rating'] = array(
 			'type'  => 'checkbox',
-			'label' => __( 'Rating Mode' ),
+			'label' => __( 'Rating & Reviews mode' ),
 			'value' => _gigParam( $values['rating'], 0 ),
+			'desc'  => __( 'Checking this button will change the mode of the Comment plugin to Rating & Reviews. Please make sure that the Category ID defined below is set to Rating & Reviews mode in Gigya platform.' )
 	);
 
 	$form['categoryID'] = array(
 			'type'  => 'text',
 			'label' => __( 'Category ID' ),
 			'value' => _gigParam( $values['categoryID'], '' ),
-			'desc'  => __( "The category ID on 'Comments category name' at Gigya's settings" ) . ' ' . '<a href=https://platform.gigya.com/Site/partners/Settings.aspx#cmd=Settings.CommentsSetup>' . __( 'here' ) . '</a>'
+			'desc'  => __( "Copy the ID under 'Comments category name' from Gigya platform." )
+	);
+
+	$form['position'] = array(
+			'type'    => 'select',
+			'options' => array(
+					'under' => __( 'Under Post' ),
+					'none'  => __( 'None' )
+			),
+			'label'   => __( 'Set the position of the Comments in a post page' ),
+			'value'   => _gigParam( $values['position'], 'under' ),
+			'desc'    => sprintf( __( 'You can also add and position Gigya Comments using the %s settings page.' ), '<a href="' . admin_url( 'widgets.php' ) . '">' . __( 'Widgets' ) . '</a>' )
 	);
 
 	$form['advanced'] = array(
