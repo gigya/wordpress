4160c7cfd44607025dcf8c1bb3c0b3fba479a8b0
Ynhockey <ynhockey@gmail.com>
2017-10-19 15:32:14 +0300

    Deleted wordpress.git dir

--- wordpress.git/tags/v.5.1-deploy/admin/tpl/formEl-hidden.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Template - Hidden-field form element for Gigya settings pages.
- * Render with @see _gigya_render_tpl().
- */
-?>
-
-<input type="hidden" name="<?php echo $id; ?>" value="<?php echo $value; ?>">
