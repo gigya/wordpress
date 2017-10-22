b4131196fe2df136bf1a1874a27d7b3aee92211d
Ynhockey <ynhockey@gmail.com>
2017-10-22 17:06:36 +0300

    Minor commit: Fixing JavaScript bad practices

--- gigya.js
+++ gigya.js
@@ -1,70 +1,64 @@
 var GigyaWp = GigyaWp || {};
 
-(function ( $ ) {
+(function ($) {
 
 // --------------------------------------------------------------------
 
-  window.__gigyaConf = gigyaParams;
+	window.__gigyaConf = gigyaParams;
 
 // --------------------------------------------------------------------
 
-  $( document ).ready( function () {
-    // jQueryUI dialog element.
-    $( 'body' ).append( '<div id="dialog-modal"></div>' );
+	$(document).ready(function () {
+		// jQueryUI dialog element.
+		$('body').append('<div id="dialog-modal"></div>');
 
-    GigyaWp.logout = function ( response ) {
-      location.replace( gigyaParams.logoutUrl );
-    };
-  } );
+		GigyaWp.logout = function (response) {
+			location.replace(gigyaParams.logoutUrl);
+		};
+	});
 
-  GigyaWp.userLoggedIn = function (response) {
-      GigyaWp.loggedUser = response.user.UID;
-      if (GigyaWp.loggedUser.length == 0) {
-          location.replace( gigyaParams.logoutUrl );
-      }
-      return GigyaWp.loggedUser;
-  };
-
-// --------------------------------------------------------------------
-
-  GigyaWp.errHandle = function ( errEvent ) {
-//    console.log( errEvent );
-    return false;
-  };
+	GigyaWp.userLoggedIn = function (response) {
+		GigyaWp.loggedUser = response.user.UID;
+		if (GigyaWp.loggedUser.length === 0) {
+			location.replace(gigyaParams.logoutUrl);
+		}
+		return GigyaWp.loggedUser;
+	};
 
 // --------------------------------------------------------------------
 
-  GigyaWp.redirect = function () {
-    if ( location.pathname.indexOf( 'wp-login.php' ) != -1 ) {
-      // Redirect after login page.
-      if ( typeof gigyaLoginParams != 'undefined' ) {
-        location.replace( gigyaLoginParams.redirect );
-      }
-      else if ( typeof gigyaRaasParams != 'undefined' ) {
-        location.replace( gigyaRaasParams.redirect );
-      }
-    }
-    else {
-      // Refresh.
-      location.reload();
-    }
-  };
-    GigyaWp.getEssentialParams = function(gigyaObj) {
-        var esData = {};
-        var primitive = ['string', 'number', 'boolean'];
-        $.each(gigyaObj.response, function(key, val) {
-            if ($.inArray($.type(val), primitive) >= 0) {
-                esData[key] = val;
-            }
-        });
-        return esData;
-    };
+	GigyaWp.errHandle = function (errEvent) {
+		return false;
+	};
 
 // --------------------------------------------------------------------
 
+	GigyaWp.redirect = function () {
+		if (location.pathname.indexOf('wp-login.php') !== -1) {
+			// Redirect after login page.
+			if (typeof gigyaLoginParams !== 'undefined') {
+				location.replace(gigyaLoginParams.redirect);
+			}
+			else if (typeof gigyaRaasParams !== 'undefined') {
+				location.replace(gigyaRaasParams.redirect);
+			}
+		}
+		else {
+			// Refresh.
+			location.reload();
+		}
+	};
+	GigyaWp.getEssentialParams = function (gigyaObj) {
+		var esData = {};
+		var primitive = ['string', 'number', 'boolean'];
+		$.each(gigyaObj.response, function (key, val) {
+			if ($.inArray($.type(val), primitive) >= 0) {
+				esData[key] = val;
+			}
+		});
+		return esData;
+	};
 
+// --------------------------------------------------------------------
 
-
-
-})( jQuery );
-
+})(jQuery);
