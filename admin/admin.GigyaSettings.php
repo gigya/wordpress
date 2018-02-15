99cd53163860f6beaf8eceed241a5827386c6608
Ynhockey <ynhockey@gmail.com>
2018-02-15 13:08:59 +0200

    Fixed JavaScript error that ruined validation

--- admin/admin.GigyaSettings.php
+++ admin/admin.GigyaSettings.php
@@ -135,7 +135,7 @@ class GigyaSettings {
 		echo '<form class="gigya-settings" action="options.php" method="post">'.PHP_EOL;
 		echo '<input type="hidden" name="action" value="gigya_settings_submit">'.PHP_EOL;
 
-		wp_nonce_field( 'update-options' );
+		wp_nonce_field( 'update-options', 'update_options_nonce' );
 		wp_nonce_field( 'wp_rest', 'wp_rest_nonce' );
 		settings_fields( $page . '-group' );
 		do_settings_sections( $page );
