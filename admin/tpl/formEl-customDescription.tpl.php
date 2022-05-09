75729a5937b7ccdfe97922f38b8f26179f826545
Shaharzilber <shaharzillber@gmail.com>
2022-05-09 16:50:59 +0300

    *changing the htacces file place to the right place *fixing bug in custom description template

--- admin/tpl/formEl-customDescription.tpl.php
+++ admin/tpl/formEl-customDescription.tpl.php
@@ -1,12 +1,12 @@
-<div class="custom-description <?php echo ( isset( $class ) ) ? $class : ''; ?> <?php echo ( isset( $depends_on ) ) ? 'gigya-depends-on' : ''; ?>"
-	<?php echo ( isset( $depends_on ) ) ? 'data-depends-on="' . htmlspecialchars( json_encode( $depends_on ), ENT_QUOTES, 'UTF-8' ) . '"' . ( empty( $display ) ? ' style="display:none;"' : '' ) :
-		''; ?>>
-	<?php if ( ! empty( $small ) ): ?>
-
+<div class="custom-description	<?php echo ( isset( $var['class'] ) ) ? $var['class'] : ''; ?> <?php echo ( isset( $var['depends_on'] ) ) ? 'gigya-depends-on' : ''; ?>"
+<?php echo ( isset( $var['depends_on'] ) ) ? 'data-depends-on="' . htmlspecialchars( json_encode( $var['depends_on'] ), ENT_QUOTES, 'UTF-8' ) . '"' . ( empty( $var['display'] ) ? ' style="display:none;"' : '' ) : ''; ?>
+>
+<?php if ( ! empty( $var['small'] ) ): ?>
 	<small>
 		<?php endif; ?>
-		<?php echo $desc; ?>
-		<?php if ( ! empty( $small ) ): ?>
+		<?php  if(isset ($var['desc'])){echo $var['desc'];}; ?>
+		<?php if ( ! empty( $var['small'] ) ): ?>
 	</small>
 <?php endif; ?>
+
 </div>
